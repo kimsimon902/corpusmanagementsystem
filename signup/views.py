9e195fe30a5d89c05318cac598a6837b3b0815b7
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-10-03 19:10:26 +0800

    Update views.py
    Troy tested if an update on the code will be reflected to the live website

--- signup/views.py
+++ signup/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Maron testing")
+    return HttpResponse("Maron testing then Troy testing")
 
 
