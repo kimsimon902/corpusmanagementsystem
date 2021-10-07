582dd579467a5a3cced8677d1dc3e95b5e0f820e
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-10-07 19:04:40 +0800

    new

--- signup/views.py
+++ signup/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,8 +9,14 @@ def home(request):
    return HttpResponse("Testing website")
 
 def registerView(request):
-    form = UserCreationForm()
-    context = {'form'}
-    return render(request, 'registration/register.html')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html',{'form':form})
 
 
