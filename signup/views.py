b15f85a1ce1ef04426b87a6d185f309acef85bc0
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-10-07 16:44:57 +0800

    Update views.py

--- signup/views.py
+++ signup/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Tutorial how to test code on website")
+    return HttpResponse("Testing website")
 
 def registerPage(request):
     form = UserCreationForm()
