cb9686601223482334194b8876c4a54bc0c69c0f
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-10-07 20:33:07 +0800

    n

--- signup/urls.py
+++ signup/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns =[
     path('',views.home, name='home'),
     #path('login/',views.home, name='home'),
-    path('register/',views.registerView, name='register_url'),
+    path('register/',views.registerView, name='register'),
+    path('profile/', views.profile, name='profile'),
+    #path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
+    #path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
