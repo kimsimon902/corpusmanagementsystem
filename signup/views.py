c8532346e574fe4b654d539c42c161c174cc2110
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-10-07 16:35:41 +0800

    new

--- signup/views.py
+++ signup/views.py
@@ -1,9 +1,16 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 
 def home(request):
     return HttpResponse("Tutorial how to test code on website")
+
+def registerPage(request):
+    form = UserCreationForm()
+    context = {'form'}
+    return render(request, 'templates/register.html', context)
 
 
