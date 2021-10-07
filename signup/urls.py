b01274ce6c72adf2e050fb75f513b9b093117c94
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-10-07 21:00:02 +0800

    Update urls.py

--- signup/urls.py
+++ signup/urls.py
@@ -5,7 +5,4 @@ from . import views
 urlpatterns =[
     path('',views.home, name='home'),
     path('register/',views.registerView, name='register'),
-    #path('profile/', views.profile, name='profile'),
-    #path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
-    #path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
