3400204efbd0fcacaccdec69b4f8a73a6a155b8e
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-10-07 20:17:29 +0800

    new

--- signup/urls.py
+++ signup/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns =[
-    path('home/',views.home, name='home'),
+    path('',views.home, name='home'),
     #path('login/',views.home, name='home'),
     path('register/',views.registerView, name='register_url'),
 ]
