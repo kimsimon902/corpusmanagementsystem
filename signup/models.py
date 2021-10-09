f494c868f1c4c1503c67d27b45b49deb8a38565b
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-10-09 15:06:40 +0800

    new

--- signup/models.py
+++ signup/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Userreg(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "auth_user"
--- signup/views.py
+++ signup/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from .forms import UserRegisterForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .models import Userreg
 
 # Create your views here.
 
@@ -11,16 +12,28 @@ def home(request):
    return render(request, 'registration/home.html')
 
 def registerView(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Hi {username}, your account was created successfully')
+    if request.method=='POST':
+        if request.POST.get('username') and request.POST.get('password') and request.POST.get('first_name') and request.POST.get('last_name'):
+            saverecord = Userreg()
+            saverecord.username = request.POST.get('username')
+            saverecord.password = request.POST.get('password')
+            saverecord.first_name = request.POST.get('first_name')
+            saverecord.last_name = request.POST.get('last_name')
+            saverecord.save()
+            messages.success(request, "Your Account Was Successfully Created")
             return redirect('home')
     else:
-        form = UserRegisterForm()
+            return render(request, 'registration/register.html')
+    # if request.method == "POST":
+    #     form = UserRegisterForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         username = form.cleaned_data.get('username')
+    #         messages.success(request, f'Hi {username}, your account was created successfully')
+    #         return redirect('home')
+    # else:
+    #     form = UserRegisterForm()
 
-    return render(request, 'registration/register.html',{'form':form})
+    # return render(request, 'registration/register.html',{'form':form})
 
 
