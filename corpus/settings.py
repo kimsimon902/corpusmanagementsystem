758af2ccaa320621b2a73197c96fc75fcc4ef217
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-10-18 16:01:52 +0800

    added static_root

--- corpus/settings.py
+++ corpus/settings.py
@@ -138,8 +138,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
+
 # STATIC_URL = '/static/'
-STATIC_URL = '/home/simonkim902/corpusmanagementsystem/static/'
+#STATIC_URL = '/home/simonkim902/corpusmanagementsystem/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
