7896962c55ed272e6c4f92976e8e0c47ac0d2ebb
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-10-18 17:40:23 +0800

    run python manage.py collectstatic

--- corpus/settings.py
+++ corpus/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = 'signup/static/'
 
 
@@ -147,7 +148,7 @@ STATIC_URL = 'signup/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    #os.path.join(BASE_DIR, 'static')
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
