849014f0e475b652da3cafddac06cb271ec26674
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-10-18 15:47:54 +0800

    revert static url
    try to fix css not working

--- corpus/settings.py
+++ corpus/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/signup/static/'
+# STATIC_URL = '/signup/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
