3279755e80c02fafb94833190e21d1e20fc5ce18
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-10-18 16:57:00 +0800

    change static_root

--- corpus/settings.py
+++ corpus/settings.py
@@ -138,8 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'corpus/static')
+STATIC_ROOT = '/home/simonkim902/corpusmanagementsystem/corpus/static'
 STATIC_URL = '/static/'
 
 # STATIC_URL = '/static/'
