87fc5a259e17934199900d768a96a535f88b4abc
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-10-18 17:43:04 +0800

    fixed static url

--- corpus/settings.py
+++ corpus/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = 'signup/static/'
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
