82aef2d62610f59b8422f054d30f437ca50d5937
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-11-01 21:05:23 +0800

    new

--- signup/views.py
+++ signup/views.py
@@ -96,4 +96,14 @@ def PublicationPage(request, id):
     return render(request, 'publication.html',{'publication':results})
 
 def uploadLiterature(request):
-    return render(request,'upload.html')
+    if request.method=='POST':
+        if request.POST.get('title') and request.POST.get('author') and request.POST.get('url'):
+            savepub = publications()
+            savepub.title = request.POST.get('title')
+            savepub.author = request.POST.get('author')
+            savepub.url = request.POST.get('url')
+            savepub.save()
+            messages.success(request, "Your Account Was Successfully Created")
+            return redirect('/')#render(request, 'registration/login.html')
+    else:
+            return render(request, 'upload.html')
