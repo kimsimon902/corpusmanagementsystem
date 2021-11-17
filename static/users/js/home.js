92fa87a219f673d62996d51123150d072dbfaa06
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-11-17 21:59:30 +0800

    collect static

--- static/users/js/home.js
+++ static/users/js/home.js
@@ -18,6 +18,7 @@ function clickEditOnAnnotation() {
 
     var save = document.getElementById("pubsAnnotation_btnSave");
     var cancel = document.getElementById("pubsAnnotation_btnCancel");
+    var del = document.getElementById("pubsAnnotation_btnDelete");
     if (save.style.display === "none" && cancel.style.display === "none") {
       save.style.display = "inline";
       cancel.style.display = "inline";
@@ -28,14 +29,28 @@ function clickEditOnAnnotation() {
 
     var edit = document.getElementById("pubsAnnotation_btnEdit");
     edit.style.display = "none";
+    del.style.display = "none";
 }
 
 function clickCancelAnnotation(){
     var edit = document.getElementById("pubsAnnotation_btnEdit");
     var save = document.getElementById("pubsAnnotation_btnSave");
     var cancel = document.getElementById("pubsAnnotation_btnCancel");
+    var del = document.getElementById("pubsAnnotation_btnDelete");
     edit.style.display = "inline";
+    del.style.display = "inline";
     save.style.display = "none";
     cancel.style.display = "none";
     document.getElementById("pubsAnnotationTextArea").setAttribute('readonly',true);
+}
+
+function toggleAnnotation(){
+    var card = document.getElementById("annotationCard");
+    if (card.style.display === "inline"){
+        card.style.display = "none";
+        document.getElementById("pubsAnnotationTextArea").setAttribute('class','far fa-clipboard');
+    } else {
+        card.style.display = "inline";
+        document.getElementById("pubsAnnotationTextArea").setAttribute('class','fas fa-clipboard');
+    }
 }
