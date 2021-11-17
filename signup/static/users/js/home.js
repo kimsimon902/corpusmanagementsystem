f5f0158ab452d60f12f4696998a8046941c28d10
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-11-17 21:57:52 +0800

    change function name

--- signup/static/users/js/home.js
+++ signup/static/users/js/home.js
@@ -44,7 +44,7 @@ function clickCancelAnnotation(){
     document.getElementById("pubsAnnotationTextArea").setAttribute('readonly',true);
 }
 
-function clickShowHideAnnotation(){
+function toggleAnnotation(){
     var card = document.getElementById("annotationCard");
     if (card.style.display === "inline"){
         card.style.display = "none";
