b4a418b131e617be73e9a83e34c17718da2565c5
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-11-17 22:19:41 +0800

    fix main.js

--- signup/static/users/js/main.js
+++ signup/static/users/js/main.js
@@ -2,7 +2,7 @@ function toggleAnnotationButton(){
     var card = document.getElementById("annotationCard");
     if (card.style.display === "inline"){
         card.style.display = "none";
-        document.getElementById("pubsAnnotationTextArea").setAttribute('class','far fa-clipboard');
+        document.getElementById("annotationButton").setAttribute('class','far fa-clipboard');
     } else {
         card.style.display = "inline";
         document.getElementById("annotationButton").setAttribute('class','fas fa-clipboard');
