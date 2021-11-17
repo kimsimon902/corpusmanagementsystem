fd1b28d9335f8e7fc382534b75d0fde85735bcae
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-11-17 21:46:21 +0800

    Update home.js

--- signup/static/users/js/home.js
+++ signup/static/users/js/home.js
@@ -48,7 +48,9 @@ function clickShowHideAnnotation(){
     var card = document.getElementById("annotationCard");
     if (card.style.display === "inline"){
         card.style.display = "none";
+        document.getElementById("pubsAnnotationTextArea").setAttribute('class','far fa-clipboard');
     } else {
         card.style.display = "inline";
+        document.getElementById("pubsAnnotationTextArea").setAttribute('class','fas fa-clipboard');
     }
 }
