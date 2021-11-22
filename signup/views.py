7fe0d76fd2f36f098253d0069648bf43dbdd523d
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-11-22 11:14:28 +0800

    Fix

--- signup/views.py
+++ signup/views.py
@@ -338,7 +338,7 @@ def PublicationPageInFolder(request, folderid, username, id):
     return render(request, 'publication-folder.html', {'publication':results, 'annotations':annotation, 'my_folders':my_folders, 'in_bookmark':in_bookmark, 'not_bookmark':not_bookmark, 'folderID': folderid, 'pubID': id})
 
 
-def PublicationPageAnnotate(request, id):
+def PublicationPageAnnotate(request, username, folderID, id):
     results = publications.objects.filter(id=id)
     if (request.user):
         author= request.session['username']
