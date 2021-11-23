c92637cc5e4fb803ad2fe38b52a4ada580ea1e41
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-11-23 21:50:38 +0800

    Update views.py

--- signup/views.py
+++ signup/views.py
@@ -322,7 +322,7 @@ def PublicationPage(request, id):
 
 
     in_bookmark = bookmarks_folder.objects.filter(id__in=bookmark_value)
-    not_bookmark = bookmarks_folder.objects.filter(id__in=folders_value).exclude(id__in=bookmark_value)
+    not_bookmark = bookmarks_folder.objects.exclude(id__in=bookmark_value).filter(id__in=folders_value)
 
 
     return render(request, 'publication.html', {'publication':results, 'annotations':annotation, 'my_folders':my_folders, 'in_bookmark':in_bookmark, 'not_bookmark':not_bookmark})
