d7143fa065fc0ba88e9c4293c3bdc5f845e08c0e
kylelagado17 <kyle_lagado@dlsu.edu.ph>
2021-11-23 21:35:43 +0800

    Collaborators

--- signup/views.py
+++ signup/views.py
@@ -322,7 +322,7 @@ def PublicationPage(request, id):
 
 
     in_bookmark = bookmarks_folder.objects.filter(id__in=bookmark_value)
-    not_bookmark = bookmarks_folder.objects.exclude(id__in=bookmark_value)
+    not_bookmark = bookmarks_folder.objects.filter(publicationID=id, folderID__in=folders_value).exclude(id__in=bookmark_value)
 
 
     return render(request, 'publication.html', {'publication':results, 'annotations':annotation, 'my_folders':my_folders, 'in_bookmark':in_bookmark, 'not_bookmark':not_bookmark})
@@ -345,7 +345,7 @@ def PublicationPageInFolder(request, folderid, username, id):
     bookmark_value = bookmarks.objects.filter(publicationID=id, folderID__in=folders_value).values('folderID')
 
     in_bookmark = bookmarks_folder.objects.filter(id__in=bookmark_value)
-    not_bookmark = bookmarks_folder.objects.exclude(id__in=bookmark_value)
+    not_bookmark = bookmarks_folder.objects.filter(publicationID=id, folderID__in=folders_value).exclude(id__in=bookmark_value)
 
     collabs = collaborators.objects.filter(collab=email).values('folderID')
     shared_with_me = bookmarks_folder.objects.filter(id__in=collabs)
