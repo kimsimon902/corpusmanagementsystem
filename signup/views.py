2fd8f5a9b0f888b625ccba17d3a56bf58497153b
kylelagado17 <kyle_lagado@dlsu.edu.ph>
2021-11-23 22:17:23 +0800

    Collaborators

--- signup/views.py
+++ signup/views.py
@@ -119,7 +119,7 @@ def searchPublication(request):
             if 'ais' in libFilter and len(libFilter) == 1:
                 results = publications.objects.filter(
                     Q(title__icontains=searched) |
-                    Q(author__icontains=searched), source__icontains='ais'
+                    Q(author__icontains=searched), source__icontains='ais', status__icontains=None
             )
 
 
@@ -129,7 +129,7 @@ def searchPublication(request):
                     Q(source__icontains='ieee')
                 ).filter(
                     Q(title__icontains=searched) |
-                    Q(author__icontains=searched)
+                    Q(author__icontains=searched), status__icontains=None
                 )        
 
 
@@ -139,14 +139,14 @@ def searchPublication(request):
                     Q(source__icontains="scopus")
                 ).filter(
                     Q(title__icontains=searched) |
-                    Q(author__icontains=searched)
+                    Q(author__icontains=searched), status__icontains=None
                 )
 
 
             elif 'ieee' in libFilter and len(libFilter) == 1:
                 results = publications.objects.filter(
                     Q(title__icontains=searched) |
-                    Q(author__icontains=searched), source__icontains="ieee"
+                    Q(author__icontains=searched), source__icontains="ieee", status__icontains=None
             )
 
                 
@@ -156,19 +156,19 @@ def searchPublication(request):
                     Q(source__icontains="scopus")
                 ).filter(
                     Q(title__icontains=searched) |
-                    Q(author__icontains=searched)
+                    Q(author__icontains=searched), status__icontains=None
                 )
 
 
             elif 'scopus' in libFilter and len(libFilter) == 1:
                 results = publications.objects.filter(
                     Q(title__icontains=searched) |
-                    Q(author__icontains=searched), source__icontains="scopus"
+                    Q(author__icontains=searched), source__icontains="scopus", status__icontains=None
             )
             else:
                 results = publications.objects.filter(
                     Q(title__icontains=searched) |
-                    Q(author__icontains=searched)
+                    Q(author__icontains=searched), status__icontains=None
             )
 
             xlist =     list(results)
@@ -186,39 +186,39 @@ def searchPublication(request):
         elif searchFilter == "title":
             
             if 'ais' in libFilter and len(libFilter) == 1:
-                results = publications.objects.filter(title__icontains=searched,source__icontains="ais")
+                results = publications.objects.filter(title__icontains=searched,source__icontains="ais", status__icontains=None)
 
 
             elif 'ais' in libFilter and 'ieee' in libFilter and len(libFilter) == 2:
                 results = publications.objects.filter( 
                     Q(source__icontains="ais") |
-                    Q(source__icontains="ieee"), title__icontains=searched
+                    Q(source__icontains="ieee"), title__icontains=searched, status__icontains=None
                 )
 
 
             elif 'ais' in libFilter and 'scopus' in libFilter and len(libFilter) == 2:
                 results = publications.objects.filter( 
                     Q(source__icontains="ais") |
-                    Q(source__icontains="scopus"), title__icontains=searched
+                    Q(source__icontains="scopus"), title__icontains=searched, status__icontains=None
                 )
 
                 
             elif 'ieee' in libFilter and len(libFilter) == 1:
-                results = publications.objects.filter(title__icontains=searched,source__icontains="ieee")
+                results = publications.objects.filter(title__icontains=searched,source__icontains="ieee", status__icontains=None)
 
 
             elif 'ieee' in libFilter and 'scopus' in libFilter and len(libFilter) == 2:
                 results = publications.objects.filter( 
                     Q(source__icontains="ieee") |
-                    Q(source__icontains="scopus"), title__icontains=searched
+                    Q(source__icontains="scopus"), title__icontains=searched, status__icontains=None
                 )
                 
             elif 'scopus' in libFilter and len(libFilter) == 1:
-                results = publications.objects.filter(title__icontains=searched, source__icontains="scopus")
+                results = publications.objects.filter(title__icontains=searched, source__icontains="scopus", status__icontains=None)
 
 
             else:
-                results = publications.objects.filter(title__icontains=searched)
+                results = publications.objects.filter(title__icontains=searched, status__icontains=None)
  
 
             xlist =     list(results)
@@ -232,25 +232,25 @@ def searchPublication(request):
         elif searchFilter == "author":
 
             if 'ais' in libFilter and len(libFilter) == 1:
-                results = publications.objects.filter(author__icontains=searched,source__icontains="ais")
+                results = publications.objects.filter(author__icontains=searched,source__icontains="ais", status__icontains=None)
 
 
             elif 'ais' in libFilter and 'ieee' in libFilter and len(libFilter) == 2:
                 results = publications.objects.filter(
                     Q(source__icontains="ais")|
-                    Q(source__icontains="ieee"), author__icontains=searched
+                    Q(source__icontains="ieee"), author__icontains=searched, status__icontains=None
                 )
 
 
             elif 'ais' in libFilter and 'scopus' in libFilter and len(libFilter) == 2:
                 results = publications.objects.filter(
                     Q(source__icontains="ais")|
-                    Q(source__icontains="scopus"), author__icontains=searched
+                    Q(source__icontains="scopus"), author__icontains=searched, status__icontains=None
                 )
 
 
             elif 'ieee' in libFilter and len(libFilter) == 1:
-                results = publications.objects.filter(author__icontains=searched,source__icontains="ieee")
+                results = publications.objects.filter(author__icontains=searched,source__icontains="ieee", status__icontains=None)
 
 
             elif 'ieee' in libFilter and 'scopus' in libFilter and len(libFilter) == 2:
@@ -260,11 +260,11 @@ def searchPublication(request):
                 )
 
             elif 'scopus' in libFilter and len(libFilter) == 1:
-                results = publications.objects.filter(author__icontains=searched, source__icontains="scopus")    
+                results = publications.objects.filter(author__icontains=searched, source__icontains="scopus", status__icontains=None)    
 
 
             else:
-                results = publications.objects.filter(author__icontains=searched)
+                results = publications.objects.filter(author__icontains=searched, status__icontains=None)
 
             
 
