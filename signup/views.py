382ad1e956a677df2286e12089a343c82a3afcef
unknown <simon_kim@dlsu.edu.ph>
2021-11-24 15:16:54 +0800

    try

--- signup/views.py
+++ signup/views.py
@@ -404,12 +404,13 @@ def PublicationPage(request, id):
     keyword_count = []
     xlist = list(results)
 
-    for pubkey in publication_keys:
-        if id == pubkey.publication_id:
-            for pubid in keywords_list:
-                if pubkey.keywords_id == pubid.id:
-                    if pubid.keywordname not in keyword_results:
-                        keyword_results.append(pubid.keywordname)
+    for publication in xlist:
+        for pubkey in publication_keys:
+            if publication.id == pubkey.publication_id:
+                for pubid in keywords_list:
+                    if pubkey.keywords_id == pubid.id:
+                        if pubid.keywordname not in keyword_results:
+                            keyword_results.append(pubid.keywordname) 
 
     return render(request, 'publication.html', {'publication':results, 'annotations':annotation, 'my_folders':my_folders, 'in_bookmark':in_bookmark, 'not_bookmark':not_bookmark, 'pubID': id, 'collaborators':collaborator, 'collabs':collabs, 'sharedfolders': shared_folders, 'sharedbookmarks': shared_folders_bookmarks, 'sharedpubs':shared_folders_pubs, 'inshared':in_shared_bookmark, 'notinshared':not_shared_bookmark, 'keyword_results':keyword_results})
 
