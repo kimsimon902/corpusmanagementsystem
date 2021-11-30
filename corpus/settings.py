0dfd50f604bc27026a6dea835fa23ea41d291270
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-11-30 17:03:49 +0800

    Update settings.py

--- corpus/settings.py
+++ corpus/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ['simonkim902.pythonanywhere.com']
 INSTALLED_APPS = [
     'signup',
     'crispy_forms',
-    'reportlab',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
