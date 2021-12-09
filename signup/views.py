edb5f5aa711245516e0b31d3b231551daf6cfa9c
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-12-09 15:44:05 +0800

    Update views.py

--- signup/views.py
+++ signup/views.py
@@ -1014,6 +1014,9 @@ def viewAdmin(request):
 
     return render(request, 'main/adminpage.html',{'publications':results})
 
+def onFirstPage(canvas, document):
+    canvas.drawCentredString(0, 0, 'Text drawn with onFirstPage')
+
 def downloadFolderTable(request):
     email = request.session['email']
     if request.method == 'POST':
@@ -1085,7 +1088,7 @@ def downloadFolderTable(request):
         pdf.build(elems)
         buf.seek(0)
 
-        return FileResponse(buf, as_attachment=True, filename='Corpus_Table.pdf',onFirstPage=canvas.drawCentredString(0,0,'Summary For'))
+        return FileResponse(buf, as_attachment=True, filename='Corpus_Table.pdf',onFirstPage=onFirstPage)
 
 # def annotateFromPub(request):
 #     results = publications.objects.filter(id=id)
