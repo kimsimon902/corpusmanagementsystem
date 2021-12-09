ace6ef30048706b584b77ed3e4c09936c48a8a9b
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-12-09 18:07:53 +0800

    Update views.py

--- signup/views.py
+++ signup/views.py
@@ -1031,8 +1031,8 @@ def downloadFolderTable(request):
         ptext = "This is an example."
         can = canvas.Canvas(buf, pagesize=A4)
         p = Paragraph(ptext, style=styles["Normal"])
-        p.wrapOn(can, 50*mm, 50*mm)  # size of 'textbox' for linebreaks etc.
-        p.drawOn(can, 0*mm, 0*mm)
+        p.wrapOn(can, 0, 0)  # size of 'textbox' for linebreaks etc.
+        p.drawOn(can, 0, 0)
         can.save()
         data = [
             ['Title', 'Author', 'Abstract', 'URL', 'Source', 'Year']
