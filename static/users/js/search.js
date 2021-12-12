d522ab4ee8ec89ccf20e4b091d0b58546f27f0f2
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2021-12-13 00:33:56 +0800

    Fix

--- static/users/js/search.js
+++ static/users/js/search.js
@@ -1,10 +1,13 @@
 function toggleAbstract(pubID){
     var x = document.getElementById("abstract-" + pubID);
+    var y = document.getElementById("action-buttons-" + pubID);
 
     if (x.style.display === "none"){
         x.style.display = "table-row"
+        y.style.borderBottomWidth = "0px"
     }
     else {
         x.style.display = "none"
+        y.style.borderBottomWidth = "1px"
     }
 }
