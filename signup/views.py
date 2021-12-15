a1f5ed9c4ef9bbf40895a07f300a74f45cfa9aa4
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-12-15 17:27:47 +0800

    Update views.py

--- signup/views.py
+++ signup/views.py
@@ -1200,7 +1200,7 @@ def downloadFolderTable(request):
         ]
         
         for pub in getpubs:
-            data.append([KeepTogether(Paragraph(pub.title, styleN)),Paragraph(pub.author, styleN),Paragraph(pub.url, styleN),Paragraph(pub.source, styleN),Paragraph(pub.year, styleN)])
+            data.append([Paragraph(pub.title, styleN),Paragraph(pub.author, styleN),Paragraph(pub.url, styleN),Paragraph(pub.source, styleN),Paragraph(pub.year, styleN)])
             #data.append([KeepTogether(Paragraph(pub.title, styleN)),KeepTogether(Paragraph(pub.title, styleN)),KeepTogether(Paragraph(pub.title, styleN)),KeepTogether(Paragraph(pub.title, styleN)),KeepTogether(Paragraph(pub.title, styleN)),KeepTogether(Paragraph(pub.title, styleN))])
             #data.append([Paragraph(pub.title,styles['Normal']),pub.author,'Title','Title','Title','Title'])
         
