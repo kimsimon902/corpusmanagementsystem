a3813ce9ba2f461e7db421442913d85a35500ba1
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-12-15 17:49:59 +0800

    Update views.py

--- signup/views.py
+++ signup/views.py
@@ -1251,7 +1251,7 @@ def myBarChart(data):
     bc.categoryAxis.labels.dx = 8
     bc.categoryAxis.labels.dy = -2
 
-    bc.categoryAxis.categoryNames = 'Trial1 Trial2 Trial3 Trial4 Trial5'
+    bc.categoryAxis.categoryNames = ['Trial1', 'Trial2', 'Trial3', 'Trial4', 'Trial5']
 
     bc.bars[0].fillColor = colors.blue
     bc.bars[1].fillColor = colors.lightblue
