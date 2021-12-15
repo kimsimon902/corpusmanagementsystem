42b476824a03ddd9de60a73ad907d1f6c7553df1
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2021-12-15 17:54:47 +0800

    Update views.py

--- signup/views.py
+++ signup/views.py
@@ -1292,7 +1292,7 @@ def downloadFolderTable(request):
     styleN = styles['Normal']
 
     #First add the Vizard Logo
-    im = Image('\users\img\dlsu.png', width=3*inch, height=3*inch)
+    im = Image('corpusmanagementsystem\signup\static\users\img\dlsu.png', width=3*inch, height=3*inch)
     im.hAlign = 'CENTER'
     story.append(im)
 
