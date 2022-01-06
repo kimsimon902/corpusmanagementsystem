b91f923e567e27837e3978e7cfe3a7d63ae70767
unknown <simon_kim@dlsu.edu.ph>
2022-01-06 18:01:03 +0800

    module3

--- signup/views.py
+++ signup/views.py
@@ -1808,7 +1808,7 @@ def PublicationBookmark(request, id):
 
             messages.success(request, "Added to your folder")
 
-            print(search_url)
+            print(search_url + 'hello')
             # return render(request, 'publication.html', {'publication':results, 'bookmarks':bookmark, 'annotations':annotation})
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
