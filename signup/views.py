24e49a46ad723c788e6a04bb4e6ae4022243c9bd
MaronLCF <69335789+MaronLCF@users.noreply.github.com>
2022-01-25 17:53:13 +0800

    Update views.py

--- signup/views.py
+++ signup/views.py
@@ -2586,9 +2586,10 @@ def downloadFolderTable(request):
         # 3) Add borders
         ts = TableStyle(
             [
-            ('BOX',(0,0),(-1,-1),1,colors.black),
-            ('LINEBEFORE',(2,1),(2,-1),1,colors.red),
-            #('LINEABOVE',(0,2),(-1,2),1,colors.green),
+            #('BOX',(0,0),(-1,-1),1,colors.black),
+            #('LINEBEFORE',(2,1),(2,-1),1,colors.red),
+            ('LINEABOVE',(0,0),(-1,4),1,colors.black),
+            ('LINEBELOW',(0,0),(-1,4),1,colors.black),
             #('GRID',(0,0),(-1,-1),1,colors.black),
             ]
         )
