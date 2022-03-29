a3a6e5e2c0769f0e1c94415148c190b0e4ff21ee
unknown <simon_kim@dlsu.edu.ph>
2022-03-29 15:53:15 +0800

    module3

--- signup/views.py
+++ signup/views.py
@@ -72,7 +72,7 @@ newStopWords = ['div', 'divdiv', 'scholaradivdivdivdivdivdiv', 'classrowdiv', 's
 'colspan2',  'rfunction', 'rtevar', 'instanceof', 'head', 'charsetutf8', 'httpequivcontenttype', 'var', 'vc3', 'styledisplaynone', 'reference', 'ie', 'classnojs', 'langenus', 'endif', 'oldie', 'lgwfull', 'classinlineblock', 'mdblock', 
 'classcffooteritem', 'smblock', 'wwwscienceorg', 'ebookscambridgeorg', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'reference', 'jscreadingcompanionreferencesitemp', 'amp', 'dlacmorg', 'hrefjavascript', 'linkref', 'linkreveal', 'classxreflink'
 'classcitationlinkscompatibilityspan', 'classname', 'classopeninanotherwindow', 'classlink', 'citationauthorj', 'citationauthord', 'citationjournaltitlechem', 'citationauthors', 'citationauthorm', 'r', 'j', 'classsitemenuitem', 
- '23case', 'efunction', '11case', '15case', '100n', 'keyup', 'keydown', '0case', 'nullfunction', 'display', 'xploremetanav', 'n']
+ '23case', 'efunction', '11case', '15case', '100n', 'keyup', 'keydown', '0case', 'nullfunction', 'display', 'xploremetanav', 'n', '\U00100078\U00100003']
 
 all_stopwords.extend(newStopWords)
 
