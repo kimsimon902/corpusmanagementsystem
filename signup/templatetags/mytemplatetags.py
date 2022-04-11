d51b9b139b4cdd0c9b97c94a06d438ea4c24c381
unknown <simon_kim@dlsu.edu.ph>
2022-04-12 01:59:24 +0800

    pagination

--- signup/templatetags/mytemplatetags.py
+++ signup/templatetags/mytemplatetags.py
@@ -1,14 +1,10 @@
+from urllib.parse import urlencode
 from django import template
+
 register = template.Library()
 
-@register.simple_tag
-def url_replace(request, field, value):
-    d = request.GET.copy()
-    d[field] = value
-    return d.urlencode()
-
-@register.simple_tag
-def url_delete(request, field):
-    d = request.GET.copy()
-    del d[field]
-    return d.urlencode()
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    query = context['request'].GET.copy()
+    query.update(kwargs)
+    return query.urlencode()
