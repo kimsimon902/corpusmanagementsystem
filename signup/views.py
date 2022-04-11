bdd53e882f1c7568bf31ea8bfb0ea42036edac59
unknown <simon_kim@dlsu.edu.ph>
2022-04-12 01:49:46 +0800

    pagination

--- signup/views.py
+++ signup/views.py
@@ -565,20 +565,15 @@ def analytics(request, keyword):
 
 
 
-
 register = template.Library()
-
 @register.simple_tag
 def url_replace(request, field, value):
-    d = request.GET.copy()
-    d[field] = value
-    return d.urlencode()
 
-@register.simple_tag
-def url_delete(request, field):
-    d = request.GET.copy()
-    del d[field]
-    return d.urlencode()
+    dict_ = request.GET.copy()
+
+    dict_[field] = value
+
+    return dict_.urlencode()
 
 # def countYearResults(year, word):
 
