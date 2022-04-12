644d3892ee30a22ec0562ef62757e261bbbeb09d
unknown <simon_kim@dlsu.edu.ph>
2022-04-12 16:31:56 +0800

    pagination

--- signup/templatetags/mytemplatetags.py
+++ signup/templatetags/mytemplatetags.py
@@ -1,10 +1,10 @@
 from django import template
-from django.utils.http import urlencode
+
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def url_replace(context, **kwargs):
-    query = context['request'].GET.dict()
-    query.update(kwargs)
-    print("hello i am templatetag")
-    return urlencode(query)
+def query_transform(context, **kwargs):
+    query = context['request'].GET.copy()
+    for k, v in kwargs.items():
+        query[k] = v
+    return query.urlencode()
