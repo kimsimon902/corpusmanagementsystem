327ebf6ecb3fc7430a3aada9ff5b85ac2d1ad94c
unknown <simon_kim@dlsu.edu.ph>
2022-04-12 16:02:20 +0800

    pagination

--- corpus/settings.py
+++ corpus/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'templatetags'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
