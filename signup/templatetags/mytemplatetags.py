dd23963c3ef9e0d54863de080c380d0c88f34e57
unknown <simon_kim@dlsu.edu.ph>
2022-04-12 16:05:01 +0800

    pagination

--- signup/templatetags/mytemplatetags.py
+++ signup/templatetags/mytemplatetags.py
@@ -3,11 +3,11 @@ from django import template
 
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
-def param_replace(context, **kwargs):
-    d =context['request'].GET.copy()
-    for k,v in kwargs.items():
-        d[k] = v
-    for k in [k for k,v in d.items() if not v]:
-        del d[k]
-    return d.urlencode()
+@register.simple_tag
+def url_replace(request, field, value):
+
+    dict_ = request.GET.copy()
+
+    dict_[field] = value
+
+    return dict_.urlencode()
