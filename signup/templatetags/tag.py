9f570a054965540d9c898bf54873fea737f5d9da
unknown <simon_kim@dlsu.edu.ph>
2022-04-12 16:58:32 +0800

    paginnation

--- signup/templatetags/tag.py
+++ signup/templatetags/tag.py
@@ -1,14 +1,18 @@
-from urllib.parse import urlencode
 from django import template
+from django.utils.html import mark_safe
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def url_replace(context, next_page):
-    query = context['request'].GET.copy().urlencode()
-    
-    if '&page=' in query:
-        url = query.rpartition('&page=')[0]
-    else:
-        url = query
-    return f'{url}&page={next_page}'
+def url_replace(context, **kwargs):
+    query = context['request'].GET.copy()
+
+    for kwarg in kwargs:
+        try:
+            query.pop(kwarg)
+        except KeyError:
+            pass
+
+    query.update(kwargs)
+
+    return mark_safe(query.urlencode())
