d5f6934ddea0e2833a86735916ac3d0f495a58fa
unknown <simon_kim@dlsu.edu.ph>
2022-04-24 15:46:43 +0800

    search

--- signup/templatetags/tag.py
+++ signup/templatetags/tag.py
@@ -5,12 +5,8 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def url_replace(context, next_page):
-    query = context['request'].GET.copy().urlencode()
-    
-    if '&page=' in query:
-        url = query.rpartition('&page=')[0]
-    else:
-        url = query
-    return f'{url}&page={next_page}'
+def url_replace(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
 
