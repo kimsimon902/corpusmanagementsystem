002ce22a989a2c6c784e189be2040607d673ec58
unknown <simon_kim@dlsu.edu.ph>
2022-04-24 15:51:55 +0800

    search

--- signup/templatetags/tag.py
+++ signup/templatetags/tag.py
@@ -3,9 +3,9 @@ from django.utils.http import urlencode
 
 register = template.Library()
 
-
 @register.simple_tag(takes_context=True)
 def url_replace(context, **kwargs):
-    query = context['request'].GET.dict()
-    query.update(kwargs)
-    return urlencode(query)
+    query = context['request'].GET.copy()
+    for k, v in kwargs.items():
+        query[k] = v
+    return query.urlencode()
