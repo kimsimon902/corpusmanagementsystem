5c05b2126585cd5f59f69e60e84c40af80be562a
unknown <simon_kim@dlsu.edu.ph>
2022-04-24 15:08:02 +0800

    search

--- signup/views.py
+++ signup/views.py
@@ -851,7 +851,7 @@ def searchPublication(request):
 
         searched = request.GET.get('searched','')
         searchFilter = request.GET.get('filterData','')
-        
+        libFilter = request.GET.getlist('filterLib')
         yearSort = request.GET.get('sortBy', '')
         
         
