3ae9fb914813722609cd136c75ec0b627bc766bf
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2022-04-29 15:50:18 +0800

    Update views.py

--- signup/views.py
+++ signup/views.py
@@ -485,6 +485,7 @@ def authorAnalytics(request, author):
 def authorAnalyticsFilterKeyword(request, author, keyword):
     if author != None:
         publications_by_author = publications.objects.filter(author__icontains=author)
+        keywords_list = list(keywords.objects.all())
 
         pubs = []
 
@@ -539,7 +540,6 @@ def authorAnalyticsFilterKeyword(request, author, keyword):
             count+=1
 
         pubkeys_list = list(pubkeys.objects.all())
-        keywords_list = list(keywords.objects.all())
         keyword_results = []
 
         for publication in keywordFilteredPubs:
