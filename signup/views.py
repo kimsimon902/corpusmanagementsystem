567df557b9c1dbc11b483dd756d2e036cf37806c
unknown <simon_kim@dlsu.edu.ph>
2022-05-14 17:58:48 +0800

    search

--- signup/views.py
+++ signup/views.py
@@ -1461,8 +1461,9 @@ def searchPublication(request):
         if len(libFilter) > 0: 
             if "[" in libFilter[0]:
                  temp = str(libFilter).replace('[','').replace(']','').replace('\'','').replace('\"','')
-                 print(temp)
+                
                  temp2 = temp.strip()
+                 print(temp2)
                  libFilter = []
                  libFilter = temp2.split(",")
 
