f14786f742e5337c5e9e07feb88de6ef4b9e6fc7
unknown <simon_kim@dlsu.edu.ph>
2022-05-14 15:55:09 +0800

    search

--- signup/views.py
+++ signup/views.py
@@ -1431,7 +1431,7 @@ def searchPublication(request):
         publications_list = list(publications.objects.all())
 
 
-        if "[" in libFilter:
+        if any(isinstance(x, list) for x in libFilter):
             libFilter = libFilter.pop()
         else:
             print('No lists in my_list')
