df20696e473a0c0b97e68065d9d6b4472258be8b
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2022-10-06 21:30:06 +0800

    Update
    Update wsgi and allowed hosts

--- corpus/wsgi.py
+++ corpus/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myapp.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'corpus.settings'
 
 application = get_wsgi_application()
