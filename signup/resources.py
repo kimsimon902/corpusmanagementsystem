5033ab96a0b0a719a3b5c800ddb972a852b091a6
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2023-02-19 22:26:10 +0800

    Update resources.py

--- signup/resources.py
+++ signup/resources.py
@@ -5,3 +5,6 @@ class PublicationResource(resources.ModelResource):
     class meta:
         model = publications
         exclude = ('id')
+        skipped_unchanged = True
+        report_skipped = True
+        import_id_fields = ('title', 'author', 'abstract', 'url', 'pdf', 'source', 'status', 'year')
