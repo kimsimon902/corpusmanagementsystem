4c5eb17dc20d07f490ca8880c5b7c52c9ff12416
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2023-02-19 21:06:24 +0800

    Delete files

--- signup/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import publications
-
-
-class PublicationForm(forms.Form):
-    class meta:
-        model = publications
-        fields = '__all__'
--- signup/resources.py
+++
@@ -1,6 +0,0 @@
-from import_export import resources
-from .models import publications
-
-class PublicationResource(resources.ModelResource):
-    class meta:
-        model = publications
